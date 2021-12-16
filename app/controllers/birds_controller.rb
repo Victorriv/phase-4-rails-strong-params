76d9cff38f7ec6ef2e10b8b7fa0c358056036b26
Victor <victorrivera714@yahoo.com>
2021-12-16 10:35:58 -0800

    commit tree e6ebac5e7fe02b643b57f2980beb0dde90040c8e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class BirdsController < ApplicationController
 
   # GET /birds
@@ -8,9 +10,16 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    #bird = Bird.create(name: params[:name], species: params[:species]) # these are passed as a hash
+    #bird = Bird.create(params.permit(:name, :species)) #permit lets you white list the params we want.\
+      
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
+    #key of name to come from params of name
   end
+ #
+
+
 
   # GET /birds/:id
   def show
@@ -22,4 +31,29 @@ class BirdsController < ApplicationController
     end
   end
 
+  private 
+
+def bird_params
+  params.permit(:name, :species)
+
+
 end
+
+
+end
+=begin
+Whenever we have a controller ex BirdController rails will generate
+a nested params hash using the attributes it finds in our bird model.
+as a nested params hash. 
+
+if we dont want this wrapped parameters. 
+  we do this 
+     wrap_parameters format: []
+     
+=end 
+
+=begin
+
+
+
+=end 
